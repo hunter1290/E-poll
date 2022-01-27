063583cdbc149179109a9fd02561939643f4e05a
Tushar Mishra <tusharmishra020@gmail.com>
2022-01-27 23:32:13 +0530

    first

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Hero() {
+  return <Main>
+
+         <Major>
+       
+         </Major>
+         <img src="../media/Moceri-Polls.jpg" alt="" />
+  </Main>;
+}
+
+const Main = styled.div`
+ 
+`;
+const Major = styled.div`
+`;
+
+export default Hero;
